621b8e34d2238def51f1b50c6d37f58ccd428593
yretes <69961361+yretes@users.noreply.github.com>
2020-09-01 14:14:57 +0200

    I finished my assignment

--- Age Assignment/Age Assignment.cpp
+++ Age Assignment/Age Assignment.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
+int age;
+
 int main()
 {
-   
+	std::cout << "What is your age?\n";
+	std::cin >> age;
+	if (age > 20) {
+		std::cout << "You are an adult";
+	}
+	else if (age < 20) {
+		std::cout << "You are a teenager.";
+	}
 }
